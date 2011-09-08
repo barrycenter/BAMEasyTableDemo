b40c8a5a59c60c0456c22b74c5eb0984a73f377d
Barry Murphy <barry@barrycenter.com>
2011-09-08 03:26:29 -0500

    Initial Commit

---
+++ BAMEasyTableDemo/BAMEasyTableDemoAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  BAMEasyTableDemoAppDelegate.h
+//  BAMEasyTableDemo
+//
+//  Created by Barry Murphy on 9/8/11.
+//  Copyright 2011 Barry Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BAMEasyTableDemoAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
